58be16fccebc2802621949248aaa90412992efb2
DeadDPie <elizabethfox434@gmail.com>
2023-06-06 16:25:01 +0700

    done

--- hw4/BST.c
+++ hw4/BST.c
@@ -5,7 +5,6 @@
 // Программа должна работать с произвольным файлом (то есть, в файле может быть больше, чем одна строка).
 // Например, на вход может быть подан файл с кодом программы или любой другой текстовый файл.\
 
-//D://clion projects//hw4//hw4.txt
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,34 +16,18 @@ typedef struct BST{
     struct BST *left;
 }BST;
 
-//strtok().strcmp().
-
-//path = "D://clion projects//hw4//hw4.txt
-
-void printTree(BST* Root){
-    if (Root != NULL) {
-        printTree(Root->left);
-
-        printf("%s ", Root->word);
-
-        printTree(Root->right);
-    }
-}
 BST* insertionInLeaf(BST* Root, char* word,int position){
     if (Root == NULL){
         BST* newRoot = (BST*)malloc(sizeof(BST));
-//        strcpy(newRoot->word, word);
         newRoot->word = word;
         newRoot->position = position;
         newRoot->left = NULL;
         newRoot->right = NULL;
         return newRoot;
     }else{
-        //if (strcmp(Root->word, word)>0){
         if (word[0] >= (Root->word)[0]){
             Root->right = insertionInLeaf(Root->right, word,position);
         }
-        //if(strcmp(Root->word, word)<0){
         else{
             Root->left = insertionInLeaf(Root->left, word,position);
         }
@@ -53,7 +36,7 @@ BST* insertionInLeaf(BST* Root, char* word,int position){
 }
 int search(BST* Root, char* word, int position) {
     if (Root == NULL) {
-        printf("?Root is empty?");
+        printf("There is no such word");
         return 0;
     }
     if (strcmp(word, Root->word) == 0) {
@@ -68,17 +51,6 @@ int search(BST* Root, char* word, int position) {
         Root->position = position;
         return search(Root->right, word, position);
     }
-//    BST* TempNode = Root;
-//    while (TempNode) {
-//        if (strcmp(TempNode->word, word) == 0)
-//            return TempNode;
-//        else if (word[0] >= (TempNode->word)[0])
-//            TempNode = TempNode->right;
-//        else
-//            TempNode = TempNode->left;
-//    }
-//
-//    return NULL;
 }
 BST* word_selection(char *path, BST* Root){
 
@@ -87,62 +59,48 @@ BST* word_selection(char *path, BST* Root){
         printf("Cannot open file");
         exit(1);
     }
-    char str[1000];
+    char str[10000];
 
-    fgets(str, 1000, file);
+    fgets(str, 10000, file);
     printf("Text from my txt file: \n%s", str);
     printf("\n--------\n");
     char *istr;
-    int position = 0;
+    int position = 1;
     istr = strtok (str," ");
-//    printf("%s", istr);
-    printf("\n");
-//    insertionInLeaf(Root, istr);
+    printf("All words from the text:\n");
     while(1){
         if (istr == NULL) break;
         printf("%s ", istr);
         Root = insertionInLeaf(Root, istr, position);
         position++;
-        istr = strtok(NULL," ");//,;.!?
+        istr = strtok(NULL," ,;.!?");
 
     }
     return Root;
 }
 int main() {
     BST* Root = NULL;
-    char path[50] = "D://clion projects//hw4//hw4.txt";
+    char path[500];
     printf("Enter name of the file with text: ");
-//    fgets(path, 50, stdin);
+    fgets(path, 50, stdin);
     printf("%s\n", path);
     int pos = strcspn(path, "\n"); // find position of first '\n' character
     if (pos != strlen(path)) path[pos] = '\0';
 
     Root = word_selection(path,Root);
-//    printTree(Root);
-    printf("\nEnter a word that you want to find: ");
-    char search_string[100];
-//    scanf("%s", search_string);
-//    printf("%s", search_string);
-    scanf("%[^\n]", search_string);
 
-    int i = 0;
-    while (search_string[i] != '\0') {
-        printf("%c", search_string[i]);
-        i++;
+    char search_string[1500];
+    printf("\n--------\n");
+    printf("\nEnter a word that you want to find: ");
+
+    fgets(search_string, sizeof(search_string), stdin);
+    if (search_string[strlen(search_string) - 1] == '\n') {
+        search_string[strlen(search_string) - 1] = '\0';
     }
 
-//    fgets(search_string, 50, stdin);
-//    int pos1 = strcspn(path, "\n"); // find position of first '\n' character
-//    if (pos1 != strlen(path)) path[pos] = '\0';
-
-//    char search_string[100] = "been";
-//    while (getchar() != '\n')
-//        ;
-
-    int word_position;
-     word_position = search(Root, search_string, 0);
-    printf("\n Position of the string that you wanted to find: %d", word_position);
+    int word_position = 0;
+    word_position = search(Root, search_string, 1);
+    printf("\nYour word is %d word", word_position);
 
     return 0;
 }
-//D://clion projects//hw4//hw4.txt
